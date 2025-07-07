7b8dc5a057ae6a4ef302ef627c14fe2d4c3dc89e
1wwg2 <vigleg777@gmail.com>
2025-07-07 13:29:42 +0300

    preparation for public repo

--- examples/Example_Stack.cpp
+++ examples/Example_Stack.cpp
@@ -1,6 +1,6 @@
-
 #include <iostream>
-#include "../MyADT/MyStack.h" 
+#include "../MyADT/MyStack.h"
+
 int main() {
     MyStack<int> stack;
 
@@ -8,26 +8,27 @@ int main() {
     stack.Push(20);
     stack.Push(30);
 
-    std::cout << "Верхний элемент: " << stack.Top() << std::endl; // ожидаем 30
+    std::cout << "Top element: " << stack.Top() << std::endl; // expect 30
 
     stack.Pop();
-    std::cout << "После Pop верхний элемент: " << stack.Top() << std::endl; // ожидаем 20
+    std::cout << "Top element after Pop: " << stack.Top() << std::endl; // expect 20
 
-    std::cout << "Размер стека: " << stack.Size() << std::endl; // ожидаем 2
+    std::cout << "Stack size: " << stack.Size() << std::endl; // expect 2
 
-    std::cout << "Стек пуст? " << (stack.IsEmpty() ? "Да" : "Нет") << std::endl;
+    std::cout << "Is stack empty? " << (stack.IsEmpty() ? "Yes" : "No") << std::endl;
 
     MyStack<int> otherStack;
     otherStack.Push(40);
     otherStack.Push(50);
 
-    std::cout << "Верхний элемент второго стека: " << otherStack.Top() << std::endl; // ожидаем 50
+    std::cout << "Top element of second stack: " << otherStack.Top() << std::endl; // expect 50
 
     stack.Swap(otherStack);
 
-    std::cout << "\nПосле обмена:" << std::endl;
-    std::cout << "Верхний элемент первого стека: " << stack.Top() << std::endl;  // ожидаем 50
-    std::cout << "Верхний элемент второго стека: " << otherStack.Top() << std::endl; // ожидаем 20
+    std::cout << "\nAfter swap:" << std::endl;
+    std::cout << "Top element of first stack: " << stack.Top() << std::endl;  // expect 50
+    std::cout << "Top element of second stack: " << otherStack.Top() << std::endl; // expect 20
 
     return 0;
 }
+
